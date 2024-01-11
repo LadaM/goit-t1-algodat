37046270d43fa76f3bec2516b1c318375c39960b
Lada <lada11lada@gmail.com>
2024-01-11 22:19:19 +0100

    task 2 palindrome testing

---
+++ src/hw_02/task_2_ palindrome_test.py
@@ -0,0 +1,47 @@
+from collections import deque
+import re
+
+
+def test_palindrome(word: str) -> bool:
+    # case of letters is irrelevant as well as spacing
+    test_word = re.sub(r"\W", "", word).lower()
+    # base case - empty string is a palindrome as well as a string with one character
+    if len(test_word) <= 1:
+        return True
+
+    word_deque = deque(maxlen=len(test_word))
+    for char in test_word:
+        word_deque.append(char)
+
+    while word_deque:
+        # ensuring that queue has at least two elements
+        if len(word_deque) <= 1:
+            return True
+        first = word_deque.popleft()
+        last = word_deque.pop()
+        if first != last:
+            return False
+
+    return True
+
+
+if __name__ == "__main__":
+    test_words = [
+        "98789",
+        "121",
+        "my name is Lada",
+        "cat",
+        "ada",
+        "adam",
+        "Mr. Owl ate my metal worm.",
+        "kayak",
+        " ",
+        "",
+        "22",
+    ]
+    for word in test_words:
+        print(
+            f"'{word}' is"
+            + (" not " if not test_palindrome(word) else "")
+            + " a palindrome"
+        )
