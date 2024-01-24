7f2f6fd77c3359301b3d0a49ee4c5c8f64773e24
Lada <lada11lada@gmail.com>
2024-01-24 09:55:55 +0100

    upd - added parsing arguments for path_from and path_to to the main function of task1

--- homeworks/hw_03/task1_file_copying.py
+++ homeworks/hw_03/task1_file_copying.py
@@ -1,8 +1,9 @@
 import os
+import sys
 from pathlib import Path
 import shutil
 
-COPY_DIR_NAME = "copy"
+COPY_DIR_NAME = "dist"
 TEST_DIR_NAME = "test_dir_task1"
 PARENT_DIR = Path(__file__).parent
 IGNORED_DIRECTORIES = {"venv", "node_modules", "bin", "build", "dist",
@@ -47,11 +48,35 @@ def copy_files_recursive(path_from, path_to):
             copy_files_recursive(entry.path, path_to)
 
 
+def path_is_directory(path):
+    return path and os.path.exists(path) and os.path.isdir(path)
+
+
 def main():
+    """
+    The main function handles the input arguments for path_from and path_to and
+    sets the default paths if no arguments are provided. It then copies files
+    recursively from the source path to the target path.
+    """
+    default_path_from = Path.joinpath(PARENT_DIR, TEST_DIR_NAME)
+    default_path_to = Path.joinpath(PARENT_DIR, COPY_DIR_NAME)
+
+    arg_path_from = None
+    arg_path_to = None
+    if len(sys.argv) > 2:
+        arg_path_from = sys.argv[1]
+        arg_path_to = sys.argv[2]
+    elif len(sys.argv) > 1:
+        arg_path_from = sys.argv[1]
+    else:
+        print("No arguments for path_from or path_to were provided")
+        print(f"Using default paths: from - {default_path_from} to - {default_path_to}")
+    
+    path_from = arg_path_from if path_is_directory(arg_path_from) else default_path_from
+    path_to = arg_path_to if path_is_directory(arg_path_to) else default_path_to
+    
     try:
-        test_dir_path = Path.joinpath(PARENT_DIR, TEST_DIR_NAME)
-        copy_target_path = Path.joinpath(PARENT_DIR, COPY_DIR_NAME)
-        copy_files_recursive(str(test_dir_path), str(copy_target_path))
+        copy_files_recursive(str(path_from), str(path_to))
     except FileNotFoundError as e:
         print(f"File not found {e.filename}")
     except PermissionError as e:
